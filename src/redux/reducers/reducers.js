5b59d7b3c4d3b23234f3325313b6940d81341769
Balazs Holpar <Balazs.Holpar@continental-corporation.com>
2019-08-01 14:15:40 +0200

    Added toast feedback implementaion

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -4,9 +4,8 @@ const initialState = {
     cards: [],
     isFetching: false,
     isAnswerInvalid: false,
-    fetchError: false,
-    uploadError: false,
-    deleteError: false,
+    toastVisible: false,
+    toastMessage: ""
 };
 
 function rootReducer(state = initialState, action) {
@@ -14,67 +13,59 @@ function rootReducer(state = initialState, action) {
         case ActionTypes.BEGIN_FETCH:
             return Object.assign({}, state, {
                 isFetching: true,
-                fetchError: false
             });      
 
         case ActionTypes.FETCH_SUCCESS:
             return Object.assign({}, state, {
                 isFetching: false,
-                fetchError: false,
                 cards: action.payload
             });
 
         case ActionTypes.FETCH_FAILURE:
             return Object.assign({}, state, {
-                fetchError: true,
                 isFetching: false  
             });     
+
+        case ActionTypes.UPLOAD_OR_UPDATE_BEGIN:
+            return Object.assign({}, state, {
+                isAnswerInvalid: false  
+            });
 
         case ActionTypes.UPLOAD_SUCCESS:
             return Object.assign({}, state, {
                 cards: [
                     ...state.cards,
                     action.payload
-                ],
-                isAnswerInvalid: false,
-                uploadError: false     
-            });
-            
-        case ActionTypes.UPLOAD_FAILURE:
-            return Object.assign({}, state, {
-                isAnswerInvalid: false,
-                uploadError: true     
+                ]
             });
             
         case ActionTypes.INVALID_ANSWER:
             return Object.assign({}, state, {
-                isAnswerInvalid: action.payload,
-                uploadError: false     
+                isAnswerInvalid: action.payload
             });
-
 
         case ActionTypes.UPDATE_SUCCESS:
             return Object.assign({}, state, {
-                cards: state.cards.map(item => (item._id === action.payload._id ? action.payload : item)),
-                uploadError: false     
-            });
-
-        case ActionTypes.UPDATE_FAILURE:
-            return Object.assign({}, state, {
-                uploadError: true     
+                cards: state.cards.map(item => (item._id === action.payload._id ? action.payload : item))   
             });
 
         case ActionTypes.DELETE_SUCCESS:
             return Object.assign({}, state, {
-                cards: state.cards.filter(item => item._id !== action.payload._id),
-                deleteError: false
-            });
-        
-        case ActionTypes.DELETE_FAILURE:
-            return Object.assign({}, state, {
-                deleteError: true
+                cards: state.cards.filter(item => item._id !== action.payload._id)
             });
          
+        case ActionTypes.SHOW_TOAST:
+            return Object.assign({}, state, {
+                toastVisible: true,
+                toastMessage: action.payload
+            });
+
+        case ActionTypes.HIDE_TOAST:
+            return Object.assign({}, state, {
+                toastVisible: false,
+                toastMessage: ""
+            });
+
         default: return state;
     }
 }
